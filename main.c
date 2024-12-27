90d2de723e819d1d9beaddf33e89f0f3c920f329
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-12-27 03:58:59 +0100

    replace 'goto' loop with 'while' loop and managed EOF input

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <sys/wait.h>
 
 /**
- * main - Entry point
- * Description : Simple shell
- * Return: Nothing (waiting to ctrl + c (abort))
- */
+* main - Entry point
+* Description : Simple shell
+* Return: Nothing (waiting to ctrl + c (abort))
+*/
 
 int main(void)
 {
@@ -19,57 +19,58 @@ int main(void)
 	pid_t child_pid;
 	int index = 0;
 
-play_again: 
+	while (1)
+	{
+		/* Waiting for input */
+		printf("%s",emoji);
 
-	/* Waiting for input */
-	printf("%s",emoji);
+		/* Read the input and check if succeed */
+		readed = getline(&string, &buffer_size, stdin);
 
-	/* Read the input and check if succeed */
-	readed = getline(&string, &buffer_size, stdin);
+		if (readed == -1)
+			{
+				printf("\nヾ(• ֊ •) Good bye !\n");
+				break;
+			}
 
-	if (readed == -1)
+		/* fork current process and check if succeed */
+		child_pid = fork();
+
+		if (child_pid == -1)
 		{
-			printf("(╯°□°）╯︵ ┻━┻ (Error getline)\n");
-			goto play_again;
+			printf("(╯°□°）╯︵ ┻━┻ (Error fork)\n");
+			continue;
 		}
 
-	/* fork current process and check if succeed */
-	child_pid = fork();
-
-	if (child_pid == -1)
-	{
-		printf("(╯°□°）╯︵ ┻━┻ (Error fork)\n");
-		goto play_again;
-	}
-
-	/* clean string (remove last \n char) */
-	for(index=0; string[index] != '\0'; index++)
-	{
-		if(string[index] == '\n')
+		/* clean string (remove last \n char) */
+		for(index=0; string[index] != '\0'; index++)
 		{
-			string[index] = '\0';
+			if(string[index] == '\n')
+			{
+				string[index] = '\0';
+			}
 		}
-	}
 
-	/* setup argv[] for execve */
-	char *argv[] = {string, NULL };
+		/* setup argv[] for execve */
+		char *argv[] = {string, NULL };
 
-	/* execute child process */
-	if (child_pid == 0)
-	{
-		/* Check and execute command if no error */
-		if (execve(argv[0], argv, NULL) == -1)
+		/* execute child process */
+		if (child_pid == 0)
 		{
-			printf("1: %s: not found\n",argv[0]);
-			goto play_again;
+			/* Check and execute command if no error */
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				printf("1: %s: not found\n",argv[0]);
+				continue;
+			}
 		}
-	}
 
-	/* waiting for child finish execution */
-	else
-	{
-			wait(NULL);
-			goto play_again;
+		/* waiting for child finish execution */
+		else
+		{
+				wait(NULL);
+				continue;
+		}
 	}
 }
 
