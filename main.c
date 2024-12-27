cd896d431e2466d44941a8f9f0c2049598fb1f9b
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-12-27 04:18:59 +0100

    refactorise code

--- main.c
+++ main.c
@@ -4,16 +4,19 @@
 #include <sys/wait.h>
 
 /**
-* main - Entry point
-* Description : Simple shell
-* Return: Nothing (waiting to ctrl + c (abort))
+* main - Entry point of the simple shell
+* Description :
+*	Display a prompt
+*	Read user input
+*	Clean string (remove \n character )
+*	Fork current process to child process and execute the command
+*	Waiting child execution and display a prompt again
+* Return: Nothing (waiting abort (ctrl+c) or EOF (ctrl+d)
 */
 
 int main(void)
 {
-	/* declare & initialize variable */
 	char *string = NULL;
-	char *emoji = "( ・‿・) > ";
 	size_t buffer_size = 0;
 	ssize_t readed;
 	pid_t child_pid;
@@ -22,55 +25,45 @@ int main(void)
 	while (1)
 	{
 		/* Waiting for input */
-		printf("%s",emoji);
+		printf("( ・‿・) > ");
 
 		/* Read the input and check if succeed */
 		readed = getline(&string, &buffer_size, stdin);
-
 		if (readed == -1)
-			{
-				printf("\nヾ(• ֊ •) Good bye !\n");
-				break;
-			}
+		{
+			printf("\nヾ(• ֊ •) Good bye !\n");
+			break;
+		}
+
+		/* clean string (remove last \n char) */
+		for (index = 0; string[index] != '\0'; index++)
+		{
+			if (string[index] == '\n')
+				string[index] = '\0';
+		}
 
 		/* fork current process and check if succeed */
 		child_pid = fork();
-
 		if (child_pid == -1)
 		{
 			printf("(╯°□°）╯︵ ┻━┻ (Error fork)\n");
 			continue;
 		}
 
-		/* clean string (remove last \n char) */
-		for(index=0; string[index] != '\0'; index++)
-		{
-			if(string[index] == '\n')
-			{
-				string[index] = '\0';
-			}
-		}
-
-		/* setup argv[] for execve */
-		char *argv[] = {string, NULL };
-
-		/* execute child process */
+		/* Execute the command in child process */
 		if (child_pid == 0)
 		{
-			/* Check and execute command if no error */
+			char *argv[] = {string, NULL };
 			if (execve(argv[0], argv, NULL) == -1)
 			{
-				printf("1: %s: not found\n",argv[0]);
+				printf("1: %s: not found\n", argv[0]);
 				continue;
 			}
 		}
 
 		/* waiting for child finish execution */
 		else
-		{
-				wait(NULL);
-				continue;
-		}
+			wait(NULL);
 	}
 }
 
