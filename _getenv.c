1d85d82e70ff68b06c2fdb9b3ae6760be85a6be6
OursBlanc42 <regniersimon@hotmail.com>
2025-01-06 15:47:51 +0100

    implement the latest corrected version of _getenv from the mini simple shell

--- _getenv.c
+++ _getenv.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "find.h"
 
 /**
@@ -11,6 +12,7 @@ char *_getenv(const char *search)
 {
 	char **ptr_env = environ;
 	char *sep_equal = NULL;
+	char *cpy_var = NULL;
 
 	/*browse the environnement with a pointer and search = in the current string*/
 	while (*ptr_env != NULL)
@@ -22,7 +24,11 @@ char *_getenv(const char *search)
 		* return the string after the separator
 		*/
 		if (sep_equal != NULL && strncmp(*ptr_env, search, strlen(search)) == 0)
-			return (sep_equal + 1);
+		{
+			cpy_var = malloc(strlen(sep_equal + 1) * sizeof(char) + 1);
+			strcpy(cpy_var, sep_equal + 1);
+			return (cpy_var);
+		}
 
 		ptr_env++;
 	}
