734e781dd655a095cf0adafdee269b70b0f2e6b9
Tefa95100 <damien.julien.95100@gmail.com>
2025-01-07 10:38:11 +0100

    add prototype and struct for function built-in

--- tamagoshell.h
+++ tamagoshell.h
@@ -3,6 +3,19 @@
 
 extern char **environ;
 
+/**
+ * struct built_in - structure for the function built-in
+ *
+ *@name_func: command use by users
+ *@function_built: function call by command
+ */
+typedef struct built_in
+{
+	char *name_func;
+	void (*function_built)(void);
+} search_built;
+
+
 char *remove_newline(char *string);
 
 int execute(char *string, char *av);
@@ -10,5 +23,11 @@ int execute(char *string, char *av);
 char **separate_arg(char *string);
 
 void free_darray(char **darray);
+
+void display_env(void);
+
+void built_exit(void);
+
+void *search_built_in(char *search);
 
 #endif
